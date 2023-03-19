3ae9eb55e0b43d87abb50721c76e8d65f6bbea89
LizaShelkovskaya0705 <116936362+LizaShelkovskaya0705@users.noreply.github.com>
2023-03-19 09:53:39 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,28 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int l = 0;
+  int r = size - 1;
+  int kolichestvo = 0;
+  while (r >= l) {
+    int middle = (r + l) / 2;
+    if (*(arr + middle) == value) {
+      kolichestvo += 1;
+      int newmiddle = middle;
+      while ((*(arr + middle + 1)) == value) {
+        kolichestvo += 1;
+        middle += 1;
+      }
+      while ((*(arr + newmiddle - 1)) == value) {
+        kolichestvo += 1;
+        newmiddle -= 1;
+      }
+      return kolichestvo;
+    } else if (*(arr + middle) > value) {
+      r = middle -1;
+    } else {
+      l = middle + 1;
+    }
+  }
+  return kolichestvo;
 }
